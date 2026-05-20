8a62619c260e66b81c00e13a6edced9907916a4d
Lux-dotcom <lucia.galli07@gmail.com>
2026-05-20 12:47:45 +0200

    lab check

--- WLS/main.cpp
+++ WLS/main.cpp
@@ -2,44 +2,67 @@
 #include "raylib.h"
 #include "vec2.hpp"
 #include <vector>
-#include <cstdlib>
+#include <random>
 #include <ctime>
 
-const int LARGHEZZA = 800;
-const int ALTEZZA = 600;
+const int LENGHT = 800;
+const int HEIGHT = 600;
 const int N = 100;
-int radius = 3;
-
-// TODO 1: parametri di simulazione.
-const float R_SEP = 50.0f;
+const int boid_radius = 3;
+const float R_VIEW = 50.0f;
+const float K_AL = 1.0f;
+const float R_SEP = 20.0f;
 const float MAX_SPEED = 200.0f;
-const float W_SEP = 250.0f;
+const float K_SE = 250.0f;
 
-struct Boid {
+struct Boid
+{
     Vec2 pos;
     Vec2 vel;
 };
 
-static float casuale(float a, float b) {
-    return a + ((float) rand() / RAND_MAX) * (b - a);
+std::default_random_engine gen;
+static float random(float min, float max)
+{
+    std::uniform_real_distribution<float> dist(min,max);
+    return dist(gen);
+   
 }
 
-static Vec2 applica_wrap(Vec2 p) {
-    if (p.x < 0) p.x += LARGHEZZA;
-    if (p.x > LARGHEZZA) p.x -= LARGHEZZA;
-    if (p.y < 0) p.y += ALTEZZA;
-    if (p.y > ALTEZZA) p.y -= ALTEZZA;
+static Vec2 respawn(Vec2 p)
+{
+    if (p.x <= 0 - boid_radius)
+    {
+        p.x += LENGHT + boid_radius;
+    }
+
+    if (p.x >= LENGHT + boid_radius)
+    {
+        p.x -= LENGHT - boid_radius;
+    }
+    if (p.y <= 0 - boid_radius)
+    {
+        p.y += HEIGHT + boid_radius;
+    }
+
+    if (p.y >= HEIGHT + boid_radius)
+    {
+        p.y -= HEIGHT - boid_radius;
+    }
     return p;
 }
 
-// TODO 2: implementa la separazione.
 
-Vec2 calcola_separazione(const std::vector<Boid> &stormo, int i) {
+Vec2 cal_steer_se(const std::vector<Boid> &stormo, int i)
+{
     Vec2 steer{0, 0};
-    for (int j = 0; j != N; ++j) {
-        if (i == j) continue;
+    for (int j = 0; j != N; ++j)
+    {
+        if (i == j)
+            continue;
         float d = norm(sub(stormo[i].pos, stormo[j].pos));
-        if (d <= R_SEP) {
+        if (d <= R_SEP)
+        {
             Vec2 v_dis = sub(stormo[i].pos, stormo[j].pos);
             steer = add(steer, mul(v_dis, 1.0f / d));
             continue;
@@ -48,57 +71,68 @@ Vec2 calcola_separazione(const std::vector<Boid> &stormo, int i) {
     return steer;
 }
 
-int main() {
-    InitWindow(LARGHEZZA, ALTEZZA, "Storni - Step 06 - Separazione");
+Vec2 cal_steer_al(const std::vector<Boid> &stormo, int i)
+{
+    Vec2 v_sum{0, 0};
+    float tot = 0;
+    for (int j = 0; j != N; ++j)
+    {
+        if (j == i)
+            continue;
+        float d = dist(stormo[i].pos, stormo[j].pos);
+        if (d <= R_VIEW)
+        {
+            v_sum = add(mul(normalize(v_sum), MAX_SPEED), stormo[j].vel);
+            tot += 1;
+        }
+    }
+    if (tot == 0)
+    {
+        return v_sum;
+    }
+    Vec2 v_cm = mul(v_sum, 1 / tot);
+    Vec2 steer_all = sub(v_cm, stormo[i].vel);
+    return steer_all;
+}
+
+int main()
+{
+    InitWindow(LENGHT, HEIGHT, "Storni - Step 06 - Separazione");
     SetTargetFPS(60);
-    srand((unsigned) time(NULL));
+    srand((unsigned)time(NULL));
 
     std::vector<Boid> stormo;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         stormo.push_back(Boid{
-            {casuale(0, LARGHEZZA), casuale(0, ALTEZZA)},
-            {casuale(-150, 150), casuale(-150, 150)}
-        });
+            {random(0, LENGHT), random(0, HEIGHT)},
+            {random(-150, 150), random(-150, 150)}});
     }
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         float dt = GetFrameTime();
 
-        // TODO 3a: calcola la sterzata di ogni boid in un vettore temporaneo.
-
-
-        // TODO 3b: applica sterzata alla velocità, fai clamp, integra posizione, wrap.
-        for (int i = 0; i < N; ++i) {
-            Vec2 steer{0, 0};
-            steer = calcola_separazione(stormo, i);
-            stormo[i].vel = add(stormo[i].vel, mul(steer, W_SEP * dt));
+        for (int i = 0; i < N; ++i)
+        {
+            Vec2 steer_se{0, 0};
+            Vec2 steer_al{0, 0};
+            steer_se = cal_steer_se(stormo, i);
+            steer_al = cal_steer_al(stormo, i);
+            stormo[i].vel = add(stormo[i].vel, add(mul(steer_se, K_SE * dt), mul(steer_al, K_AL * dt)));
             stormo[i].vel.x = std::min(stormo[i].vel.x, MAX_SPEED);
             stormo[i].vel.y = std::min(stormo[i].vel.y, MAX_SPEED);
             stormo[i].pos = add(stormo[i].pos, mul(stormo[i].vel, dt));
-
-            // TODO 1: wrap toroidale.
-            if (stormo[i].pos.x<=0-radius) {
-                stormo[i].pos.x+= LARGHEZZA+radius;
-            }
-
-            if (stormo[i].pos.x>=LARGHEZZA+radius) {
-                stormo[i].pos.x-= LARGHEZZA-radius;
-            }
-            if (stormo[i].pos.y<=0-radius) {
-                stormo[i].pos.y+= ALTEZZA+radius;
-            }
-
-            if (stormo[i].pos.y>=ALTEZZA+radius) {
-                stormo[i].pos.y-= ALTEZZA-radius;
-            }
-
+            stormo[i].pos = respawn(stormo[i].pos);
         }
 
         BeginDrawing();
         ClearBackground(BLACK);
-        for (int i = 0; i < N; ++i) {
-            DrawCircle(stormo[i].pos.x, stormo[i].pos.y, R_SEP, CLITERAL(Color){ 255, 0, 0, 50 });
-            DrawCircle(stormo[i].pos.x, stormo[i].pos.y, radius,WHITE);
+        for (int i = 0; i < N; ++i)
+        {
+            DrawCircle(stormo[i].pos.x, stormo[i].pos.y, R_SEP, CLITERAL(Color){255, 0, 0, 100});
+            DrawCircle(stormo[i].pos.x, stormo[i].pos.y, boid_radius, WHITE);
+            DrawCircle(stormo[i].pos.x, stormo[i].pos.y, R_VIEW, CLITERAL(Color){0, 200, 0, 70});
         }
         EndDrawing();
     }
